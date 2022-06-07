d8d8030892495f2639cec3704f483a4563fe441c
Fabricyo Ribeiro <69694301+fabricyoribeiro@users.noreply.github.com>
2022-06-07 10:51:31 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,52 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
+function onScroll(){
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll(){
+  if(scrollY>0){
+    document.getElementById('navigation').classList.add('scroll')
+  }else{
+    document.getElementById('navigation').classList.remove ('scroll')
+
+  }
+}
+
+function showBackToTopButtonOnScroll(){
+  if(scrollY>550){
+    document.getElementById('backToTopButton').classList.add('show')
+  }else{
+    document.getElementById('backToTopButton').classList.remove ('show')
+
+  }
+}
+
+function openMenu(){
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu(){
+  document.body.classList.remove('menu-expanded')
+}
+
+ 
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+  #home,
+  #home img,
+  #home .stats,
+  #services,
+  #services header,
+  #services .card,
+  #about,
+  #about header,
+  #about .content
+`);
